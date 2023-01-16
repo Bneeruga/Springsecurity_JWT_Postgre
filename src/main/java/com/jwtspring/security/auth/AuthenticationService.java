8e7cb5d4aa73c39294972f19374c01ef4f2c1115
bneeruga <bneeruga@buffalo.edu>
2023-01-16 13:13:30 -0500

    Controller changes

---
+++ src/main/java/com/jwtspring/security/auth/AuthenticationService.java
@@ -0,0 +1,49 @@
+package com.jwtspring.security.auth;
+
+import com.jwtspring.security.config.JwtService;
+import com.jwtspring.security.user.Role;
+import com.jwtspring.security.user.User;
+import com.jwtspring.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private  final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwToken = jwtService.generateToken(user);
+
+        return  AuthenticationResponse.builder().token(jwToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwToken = jwtService.generateToken(user);
+        return  AuthenticationResponse.builder().token(jwToken).build();
+    }
+}
